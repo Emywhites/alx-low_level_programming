22b8f9e04cd4a9d37d0989e8f6b74de5e212b078
Emywhites <emmanuelchukwuma007@gmail.com>
2023-10-27 00:44:51 -0700

    0-puts_recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,14 @@
-#include "main.h"
-
+#include "main.h
+"
 /**
  * _print_rev_recursion - Prints a string in reverse.
  * @s: Pointer to the input string.
  */
-void _print_rev_recursion(char *s)
+void _print_rev_recursion(char) 
 {
-        if (*s)
-        {
-                _print_rev_recursion(s + 1);
-                _putchar(*s);
-        }
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }

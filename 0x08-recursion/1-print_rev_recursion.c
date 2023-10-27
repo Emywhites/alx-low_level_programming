3aecc3799c54634a0aad9c68fcb7af73904e9819
Emywhites <emmanuelchukwuma007@gmail.com>
2023-10-27 00:57:26 -0700

    1-print_rev_recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,19 @@
-#include "main.h
-"
+#include "main.h"
 /**
- * _print_rev_recursion - Prints a string in reverse.
- * @s: Pointer to the input string.
+ * _print_rev_recursion - Print a string in reverse.
+ * @s: the string to reserve
+ *
+ * Return: Nothing.
  */
-void _print_rev_recursion(char) 
+void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		return;
 	}
+
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }

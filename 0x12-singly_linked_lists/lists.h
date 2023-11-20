42019fc493407c200d48562ed682265f81cfe9f2
Emywhites <emmanuelchukwuma007@gmail.com>
2023-11-20 09:00:30 -0800

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef _LINKED_
+#define _LINKED_
 
 #include <stddef.h>
 #include <stdlib.h>
@@ -26,4 +26,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif
+#endif /*LINKED*/
